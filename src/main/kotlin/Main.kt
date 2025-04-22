e32df7853f9c3e86dd117f33b4526556083996ed
Jc Cloete <jc@example.com>
2025-04-22 18:32:26 +0200

    Create test for KCEF

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,71 @@
+import androidx.compose.material.Text
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import com.multiplatform.webview.web.WebView
+import com.multiplatform.webview.web.rememberWebViewState
+import dev.datlag.kcef.KCEF
+import dev.datlag.kcef.KCEFBuilder.Download
+import dev.datlag.kcef.KCEFBuilder.Download.Builder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import kotlin.math.max
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        var restartRequired by remember { mutableStateOf(false) }
+        var downloading by remember { mutableStateOf(0F) }
+        var initialized by remember { mutableStateOf(false) }
+        val download: Download = remember { Builder().github().build() }
+        val state = rememberWebViewState("http://localhost:8081/realms/spotting_station/protocol/openid-connect/auth?client_id=spotchecker-webfrontend&response_type=code")
+
+
+        LaunchedEffect(Unit) {
+            withContext(Dispatchers.IO) {
+                KCEF.init(builder = {
+                    installDir(File("kcef-bundle"))
+
+                    progress {
+                        onDownloading {
+                            downloading = max(it, 0F)
+                        }
+                        onInitialized {
+                            initialized = true
+                        }
+                    }
+                    settings {
+                        cachePath = File("cache").absolutePath
+                    }
+
+                    download(download)
+                }, onError = {
+                    it?.printStackTrace()
+                }, onRestartRequired = {
+                    restartRequired = true
+                })
+            }
+        }
+
+        if (restartRequired) {
+            Text(text = "Restart required.")
+        } else {
+            if (initialized) {
+                WebView(state)
+            } else {
+                Text(text = "Downloading $downloading%")
+            }
+        }
+
+        DisposableEffect(Unit) {
+            onDispose {
+                KCEF.disposeBlocking()
+            }
+        }
+    }
+}
